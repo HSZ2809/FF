d80a19b3576ed5fff82b200e38fb6576f4de93fd
HSZ2809 <6255plm@gmail.com>
2021-11-27 13:34:30 +0900

    remove empty line

--- Main.cpp
+++ Main.cpp
@@ -91,7 +91,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 	g_Mng.chap[GAME] = new Game;
 	g_Mng.chap[OVER] = new Over;
 	g_Mng.chap[BATTLE] = new Battle;
-	
 	/////////////////////////////////////////
 
 	for(int i=0; i<TOTALCHAP; i++)
