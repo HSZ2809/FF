262e6d95d6cf19d39f8656b67ae784632c8eb743
HSZ2809 <6255plm@gmail.com>
2021-11-29 17:53:22 +0900

    깨진 한글 주석 복구
    계속 한글이 깨지는 증상이 발현될 수도 있습니다.

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,12 @@ const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
 const int MAX_FRAMESKIP = 5;
 int loops;
 float interpolation;
-// ���� �߰�
+// 버퍼 추가
 char buffer[128] = {0,0,0,0};
-char ch[3] = {0,0,0}; //D  strcat ���� Ʋ�� �ּ� ���� ���� �ֵ��� �������
+char ch[3] = {0,0,0}; //D  strcat 사용법 틀림 최소 널이 들어갈수 있도록 해줘야함
+
 ////////////////////////////////////
+
 LRESULT CALLBACK WndProc( HWND g_hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
@@ -32,36 +34,36 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 	RegisterClassEx( &wc ) ;
 
 	/* ---------------------------------------------------
-	/////////////////// ����� ��� ////////////////////
+	/////////////////// 정가운데 출력 ////////////////////
 
-				���α׷��� ������� �����
-				����ϰ� �� �ִ� �ڵ��Դϴ�.
+				프로그램을 모니터의 정가운데
+				출력하게 해 주는 코드입니다.
 
 	//////////////////////////////////////////////////////
-	----------------------��-��-��-��-��-----------------*/
+	----------------------요-기-서-부-터-----------------*/
 	RECT rt = {0, 0, SCREEN_WITH, SCREEN_HEIGHT};
 	int W = rt.right - rt.left;
 	int H = rt.bottom - rt.top;
 	int X = (GetSystemMetrics(SM_CXSCREEN)/2) - SCREEN_WITH/2;
 	int Y = ( (GetSystemMetrics(SM_CYSCREEN)/2) - SCREEN_HEIGHT/2 );
-	/*---------------------��-��-��-��-------------------*/
+	/*---------------------요-기-까-지-------------------*/
 
 	/*
 		int WINAPI GetSystemMetrics(
 			int nIndex
 		);
 
-		<���ڰ�>
-		SM_CXSCREEN : ȭ���� ���̸� ���� �� �ִ�.
-		SM_CYSCREEN : ȭ���� ���̸� ���� �� �ִ�.
+		<인자값>
+		SM_CXSCREEN : 화면의 넓이를 구할 수 있다.
+		SM_CYSCREEN : 화면의 높이를 구할 수 있다.
 
-		���� ȭ���� �ػ󵵸� ���� �� �ִ�. �� �ܿ��� ���ڰ������� �پ��� ���� ���� �� �ִ�.
-		�ڼ��� ������ MSDN �� GetSystemMetrics �� �����ϱ� �ٶ�
-		��, ���� ����Ϳ��� ���� ���� ��� ���� ����Ϳ� ���ؼ��� ���� ���ϱ⶧���� �� �Լ��� ����ϸ� �ȵȴ�.
-		���� ����Ϳ��� ��ü �ػ󵵸� ���ϱ� ���Ұ�� GetMonitorInfo �Լ��� �̿�����
+		현재 화면의 해상도를 구할 수 있다. 이 외에도 인자값에따라 다양한 값을 구할 수 있다.
+		자세한 사항은 MSDN 의 GetSystemMetrics 를 참고하길 바람
+		단, 다중 모니터에서 값을 구할 경우 메인 모니터에 대해서만 값을 구하기때문에 이 함수를 사용하면 안된다.
+		다중 모니터에서 전체 해상도를 구하길 원할경우 GetMonitorInfo 함수를 이용하자
 	*/
 
-	// WS_POPUP ���� �������� �����~
+	// WS_POPUP 빼면 테투리가 생긴다~
 	g_hWnd = CreateWindowEx( NULL, wc.lpszClassName, 
 		"Game", 
 		WS_EX_TOPMOST, 
@@ -79,7 +81,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 
 	ShowWindow( g_hWnd, SW_SHOW ) ;
 	UpdateWindow( g_hWnd ) ;
-	// ���콺 �Ⱥ��̰�~
+	// 마우스 안보이게~
 	//ShowCursor(FALSE);
 
 	///////////////////////////////////////////////////////////////////
@@ -152,7 +154,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 
 }
 
-// Ű �ѹ��� �����Ŵ� ���Ⱑ ����...�ƴϸ� ������.(key.cpp ���� ����Ű �ȸ԰� �ϱ� ���� ������ ��������)
+// 키 한번만 먹을거는 여기가 안전...아니면 귀찮다.(key.cpp 에서 연속키 안먹게 하기 위해 로직이 더러워짐)
 LRESULT CALLBACK WndProc( HWND g_hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 {
 	switch( uMsg )
@@ -164,7 +166,7 @@ LRESULT CALLBACK WndProc( HWND g_hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 	case WM_CHAR:
 		 ch[0] = wParam;
 		 strcat( buffer,ch);
-		 // ���ڿ� ��ġ ������...
+
 		 if (strstr(buffer, "p") != NULL)
 		 {
 
@@ -177,39 +179,33 @@ LRESULT CALLBACK WndProc( HWND g_hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 		 }
 		 else if (strstr(buffer, "s") != NULL) 
 		 {
-			 // ����Ÿ ���̽� ����
 			 sql.save();
 		 }
 
         ZeroMemory( &buffer, sizeof(buffer) );
 		break;
 
-
-	//case WM_COMM_RXCHAR:  //�ø��󿡼� �޴� �޼����� ���⸦ 
-	//	 ch[0] = wParam;
-	//	 strcat( buffer,ch);	
-	//     TRACE("REV ====== %s ========= \n\n", buffer);
-
-
-
-
+		//case WM_COMM_RXCHAR:  //시리얼에서 받는 메세지는 여기를 
+		//	 ch[0] = wParam;
+		//	 strcat( buffer,ch);	
+		//     TRACE("REV ====== %s ========= \n\n", buffer);
 	}
 	return DefWindowProc( g_hWnd, uMsg, wParam, lParam ) ;
 }
 
 /*
 
-�Լ����� : char* strstr(char* str1, const char* str2);
+함수원형 : char* strstr(char* str1, const char* str2);
 
-0) ������� : C��� : <string.h> / C++ : <cstring>
+0) 헤더파일 : C언어 : <string.h> / C++ : <cstring>
 
-1) str1���� str2�� ��ġ�ϴ� ���ڿ��� �ִ��� Ȯ���� �ϴ� �Լ�.
+1) str1에서 str2와 일치하는 문자열이 있는지 확인을 하는 함수.
 
-2) str1�� str2�� ���ڿ��� ��ġ�ϴ� ���ڿ��� ������ �ش� ��ġ�� ������(char* Ÿ��)�� ��ȯ.
+2) str1에 str2의 문자열과 일치하는 문자열이 있으면 해당 위치의 포인터(char* 타입)를 반환.
 
-3) �翬�ϰԵ� ��ġ�ϴ� ���ڿ��� ã�� ���ϸ� null pointer�� ��ȯ. (�׷��� ������ ��üũ�� ��! ���־�� ��.)
+3) 당연하게도 일치하는 문자열을 찾지 못하면 null pointer를 반환. (그렇기 때문에 널체크를 꼭! 해주어야 함.)
 
-4) ���ڿ��� ã�Ƽ�, ���ڿ��� �ٲٴ� ��쿡�� ���� ���ڿ� str1�� �迭�� ���̸� �ݵ�� �����ؾ���. �迭�� ������ ������, ū���̴ϱ�.
+4) 문자열을 찾아서, 문자열을 바꾸는 경우에는 원본 문자열 str1의 배열의 길이를 반드시 생각해야함. 배열의 범위를 넘으면, 큰일이니깐.
 
 
 */
